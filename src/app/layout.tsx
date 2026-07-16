066a457fc653a4fb24ec475846ba9526efd661f7
Mugunth <mugunth140@gmail.com>
2026-07-16 18:35:43 +0530

    feat: add Plausible Analytics tracker and Facebook Pixel for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 import "@/styles/globals.css";
 
 const inter = Inter({
@@ -56,6 +57,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${plusJakartaSans.variable}`}>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1343299310662986');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1343299310662986&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body className="antialiased flex flex-col min-h-screen">
         <Navbar />
         <main className="grow">{children}</main>
