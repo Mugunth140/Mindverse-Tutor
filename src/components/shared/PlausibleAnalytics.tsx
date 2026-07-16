90933fce72a96663e4d6ad79b627755b9f5eeed6
Mugunth <mugunth140@gmail.com>
2026-07-16 18:56:12 +0530

    feat: lazy load Plausible Analytics tracker for improved performance

--- src/components/shared/PlausibleAnalytics.tsx
+++ src/components/shared/PlausibleAnalytics.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
-import { init } from "@plausible-analytics/tracker";
 
 export function PlausibleAnalytics() {
   useEffect(() => {
-    init({
-      domain: "mindversetutor.com",
+    import("@plausible-analytics/tracker").then(({ init }) => {
+      init({
+        domain: "mindversetutor.com",
+      });
     });
   }, []);
   
